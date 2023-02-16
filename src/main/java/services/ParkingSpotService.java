8c0dbb6413a57931a7431cf0ba80b568c76047a7
eduardomanzini <eduardo_manzini@hotmail.com>
2023-02-16 12:08:37 -0300

    Projeto Criado

---
+++ src/main/java/services/ParkingSpotService.java
@@ -0,0 +1,55 @@
+package services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import models.ParkingSpotModel;
+import repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+
+	
+	final ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		super();
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+
+	 @Transactional
+	  public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+	       return parkingSpotRepository.save(parkingSpotModel);
+	    }
+		
+	  public boolean existsByLicensePlateCar(String licensePlateCar) {
+	        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	    }
+	  
+	  public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+	        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+	    }
+
+	    public boolean existsByApartmentAndBlock(String apartment, String block) {
+	        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	    }
+	    
+	    public List<ParkingSpotModel> findAll(){
+	    	return parkingSpotRepository.findAll();
+	    }
+
+		public Optional<ParkingSpotModel> findById(UUID id) {
+			return parkingSpotRepository.findById(id);
+		}
+
+		@Transactional
+		public void delete(ParkingSpotModel parkingSpotModel) {
+			parkingSpotRepository.delete(parkingSpotModel);
+			
+		}
+	    
+}
